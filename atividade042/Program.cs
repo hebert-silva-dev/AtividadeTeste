2674ec842832fe5c8feb633fb7fa0e622dba46ac
Hebert Silva <2701hsribeiro@gmail.com>
2025-07-10 16:53:58 -0300

    first commit

---
+++ atividade042/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string , int> voting = new Dictionary<string , int>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(",");
+                        string name = line[0];
+                        int voto = int.Parse(line[1]);
+                        if (voting.ContainsKey(name))
+                        {
+                            voting[name] += voto;
+                        }
+                        else
+                        {
+                            voting.Add(name, voto);
+                        }
+                    }
+                    foreach (KeyValuePair<string, int> item in voting)
+                    {
+                        Console.WriteLine($"{item.Key}, {item.Value}");
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
